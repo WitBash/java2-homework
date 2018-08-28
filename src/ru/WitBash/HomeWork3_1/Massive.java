3f2d7863fe03b5b6cb7625b364350f8a0ecdeeb5
WitBash <yr-83p@mail.ru>
2018-08-28 23:57:21 +0300

    ready lesson3 part1

--- src/ru/WitBash/HomeWork3_1/Massive.java
+++ src/ru/WitBash/HomeWork3_1/Massive.java
@@ -1,17 +1,13 @@
 package ru.WitBash.HomeWork3_1;
-
 import java.util.*;
-
 public class Massive {
     public static void main(String[] args) {
-
         String[] array = {"one", "two", "three", "four", "five", "six", "seven", "one", "four",
                 "two", "eight", "one", "nine", "seven", "ten", "six", "one", "two", "five", "three"};
         ArrayList<String> a1 = new ArrayList<String>();
         uniqueMember(array, a1);
         repeatMember(array, a1);
     }
-
     static void uniqueMember(String[] arr, ArrayList<String> a) {
         a.add(arr[0]);
         for (int i = 1; i < arr.length; i++) {
@@ -23,7 +19,6 @@ public class Massive {
         }
         System.out.println(a);
     }
-
     static void repeatMember(String[] arr, ArrayList<String> a) {
         for (int i = 0; i < a.size(); i++) {
             int count = 0;
